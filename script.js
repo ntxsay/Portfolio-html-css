00912844a9c7388b22df48059a94cc4adcf07ce9
ntxsay <ntxsay@hotmail.fr>
2024-08-06 11:47:12 -0400

    Ajout menu et section profile

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-const app = document.querySelector('#landing .landing__description');
+const menuSection = document.getElementById('menu');
+const menuIcon = document.getElementById('menuIcon');
 
-var typewriter = new Typewriter(app, {
-    loop: false,
-    delay: 5,
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('active');
+    menuSection.classList.toggle('active');
 });
 
-typewriter
-    .typeString('Formé en développement web, j’ai acquis des compétences en front-end et back-end, tout en approfondissant ma passion pour le C#.')
-    .start();
+document.getElementById('btnProfile').addEventListener('click', () => {
+    menuIcon.classList.remove('active');
+    menuSection.classList.remove('active');
+    window.location.href = '#profile';
+});
 
-/*
-new Scene({
-    "#imgAppCsharp": Scene.flipY({ x: 2 }).setDuration(5),
-    "#imgAppCsharp2": Scene.flipY({ x: 2, backside: true }).setDuration(5)
-}, {
-    selector: true,
-    iterationCount: "infinite",
-}).play();*/
+document.getElementById('btnProjects').addEventListener('click', () => {
+    menuIcon.classList.remove('active');
+    menuSection.classList.remove('active');
+    window.location.href = '#projects';
+});
+
+document.getElementById('btnContact').addEventListener('click', () => {
+    menuIcon.classList.remove('active');
+    menuSection.classList.remove('active');
+    window.location.href = '#contact';
+});
+
+
+
+
+tippy(document.getElementById('scssSkill'), {
+    content(reference) {
+        return reference.getAttribute('data-tooltip');
+    }
+});
